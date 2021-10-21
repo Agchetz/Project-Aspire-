a1b90d31897ba5ffceafd99866e2c10d48b92ca4
Chethan A <86460271+Agchetz@users.noreply.github.com>
2021-10-21 15:29:32 +0530

    Add files via upload

---
+++ client/src/app/component/userComponent/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { BaseService } from 'src/app/Shared/baseService';
+import { loginDetails } from 'src/app/Shared/interface';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+
+  private user!: any;
+  public loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private myService: BaseService,
+    private toastr: ToastrService
+  ) {}
+
+  get email() {
+    return this.loginForm.controls.email;
+  }
+
+  get password() {
+    return this.loginForm.controls.password;
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      this.myService.login(this.loginForm.value).subscribe(
+        (data) => {
+        this.checkLoginDetails(data)
+        },
+         (error) => {this.toastr.error(error.error.message,'Login unsuccessful')});
+    }else{
+      this.toastr.error('Please check Email and Password');}
+  }  
+
+  checkLoginDetails(data: loginDetails) {
+    if(data.status === 200){
+    const data1 = data.data[0].token
+    localStorage.setItem('token', data1);
+    this.user = localStorage.setItem('user', data.data[1]);
+    this.myService.userName.next(this.user);
+    this.myService.loginStatus.next(true);
+    this.toastr.success(data.message, 'Login Successfull');
+    this.router.navigate(['/home']);
+  }
+}
+  
+}
